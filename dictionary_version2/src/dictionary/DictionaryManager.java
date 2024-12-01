a17e07dc4e1a75531fb3dd308582d5fd2a95b521
ĐỖ NGỌC ĐĂNG <141601124+DANGPP@users.noreply.github.com>
2024-12-01 15:44:16 +0700

    fix lại dictionarymanager

--- dictionary_version2/src/dictionary/DictionaryManager.java
+++ dictionary_version2/src/dictionary/DictionaryManager.java
@@ -1,9 +1,12 @@
 package dictionary;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class DictionaryManager {
 
@@ -40,7 +43,7 @@ public class DictionaryManager {
     }
 
     public void loadDictionary(String fileName) throws IOException {
-        List<Word> loadedWords = FileHandler.loadFromFile(fileName);
+        List<Word> loadedWords = loadFromFile(fileName);
         for (Word word : loadedWords) {
             addWord(word); // Thêm từ vào HashMap khi tải từ điển
         }
@@ -53,6 +56,30 @@ public class DictionaryManager {
 
     public List<String> getVietnameseWords() {
         return new ArrayList<>(vietnameseToEnglish.keySet()); // Trả về danh sách từ tiếng Việt
+    }
+
+    private List<Word> loadFromFile(String fileName) {
+        List<Word> words = new ArrayList<>();
+
+        try (Scanner sc = new Scanner(new File(fileName))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] parts = line.split("\\|");
+
+                // Trim các phần tử
+                for (int i = 0; i < parts.length; i++) {
+                    parts[i] = parts[i].trim();
+                }
+
+                // Tạo đối tượng Word từ các phần tử
+                words.add(new Word(parts[0], parts[1], parts[2]));
+
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File không tìm thấy: " + e.getMessage());
+        }
+
+        return words;
     }
 
 }
---
+++ dictionary_version2/src/main/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import gui.DictionaryGUI;
+import java.io.IOException;
+
+/**
+ *
+ * @author dangt
+ */
+public class main {
+    public static void main(String[] args) {
+   
+        DictionaryGUI gui = new DictionaryGUI();
+        gui.setVisible(true);
+
+        try {
+            gui.manager.loadDictionary("dictionary.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
